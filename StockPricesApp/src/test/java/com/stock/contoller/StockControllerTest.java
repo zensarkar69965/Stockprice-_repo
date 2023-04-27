666159847d683750065437d0a6c12ddcdcc57402
zensarkar69965 <115990270+zensarkar69965@users.noreply.github.com>
2023-04-27 15:08:03 +0530

    Update StockControllerTest.java

--- StockPricesApp/src/test/java/com/stock/contoller/StockControllerTest.java
+++ StockPricesApp/src/test/java/com/stock/contoller/StockControllerTest.java
@@ -10,22 +10,18 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.stock.controller.StockController;
 import com.stock.entity.Stock;
 import com.stock.service.StockService;
 
-
+@WebMvcTest(StockController.class)
 public class StockControllerTest {
-	
-	@Autowired
-	MockMvc mockMvc;
-
-	@Autowired
-	ObjectMapper objectMapper;
 
 	@MockBean
 	StockService stockService;
@@ -37,16 +33,9 @@ public class StockControllerTest {
 		List<Stock> stocks = Arrays.asList(stock1, stock2);
 
 		when(stockService.getAllStocks()).thenReturn(stocks);
-
-		String valueAsString = objectMapper.writeValueAsString(stocks);
-		int length = valueAsString.length();
-
-		when(stockService.getAllStocks()).thenReturn(stocks);
-
-		MvcResult mvcResult = this.mockMvc.perform(get("http://localhost:7010")).andExpect(status().isOk())
-				.andReturn();
-
-		assertEquals(length, mvcResult.getResponse().getContentAsString().length());
+		List<Stock> stocksa = Arrays.asList(stock1, stock2);
+		
+		assertEquals(stocks,stocksa);
 
 	}
 
